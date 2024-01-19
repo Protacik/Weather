2605c6e5c9e4f577db4c74063f21e5ba7018bf56
protacik <protacik@gmail.com>
2024-01-18 20:04:12 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { WeatherApp } from './Components/Weather/WeatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp/>
+      
+    </div>
+  );
+}
+
+export default App;
